4533a0b7e9e32d28f8d352826873cf28438f3bef
SantinoGiordano <santinogiordano13@gmail.com>
2024-12-01 21:26:22 -0500

    removed data set from routes page and moved it into employees.tsx

--- app/api/employees/route.ts
+++ app/api/employees/route.ts
@@ -1,10 +1,7 @@
+import employees from "@/app/lib/employees/employees";
+
 export async function GET(request: Request) {
-    const employees = [
-        { id: 1, name: "Alice Johnson", position: "Software Engineer", email: "alice@techcompany.com"},
-        { id: 2, name: "Bob Smith", position: "Product Manager", email: "bob@techcompany.com"},
-        { id: 3, name: "Catherine Lee", position: "UX Designer", email: "catherine@techcompany.com"},
-        { id: 4, name: "David Wilson", position: "Data Scientist", email: "david@techcompany.com"},
-    ];
+    
 
     return new Response(JSON.stringify({ employees }), {
         status: 200,
