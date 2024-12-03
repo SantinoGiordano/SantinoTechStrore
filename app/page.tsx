d4ee2ab140a3fe5d8d74bd0f29240c44d279468c
SantinoGiordano <santinogiordano13@gmail.com>
2024-12-02 21:11:32 -0500

    added use state and fetch in trry catch

--- app/page.tsx
+++ app/page.tsx
@@ -1,18 +1,24 @@
 'use client'
 
+import { useEffect } from "react";
+
 export default function Home() {
 
  async function getEmployees(){
   try {
-  
+    const response = fetch('http://localhost:3000/api/employees')
   } catch (error) {
-    
+   console.log(error) 
   }
  }
 
+useEffect(() =>{
+  getEmployees();
+ },[])
+ 
   return (
     <div>
-      this is a test
+      
     </div>
   );
 }
