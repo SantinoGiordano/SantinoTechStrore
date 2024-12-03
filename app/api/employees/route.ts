95d97cfd345f887a31b2ac983303d1fbae78cd20
SantinoGiordano <santinogiordano13@gmail.com>
2024-12-03 11:23:58 -0500

    route.ts for employees got more data and took away employees : employees

--- app/api/employees/route.ts
+++ app/api/employees/route.ts
@@ -1,9 +1,35 @@
-import employees from "@/app/lib/employees/employees";
+// import employees from "@/app/lib/employeesData/employees";
 
 export async function GET(request: Request) {
     
+const employees = [
+    {
+      "id": 1,
+      "name": "Alice Johnson",
+      "position": "Software Engineer",
+      "email": "alice@techcompany.com"
+    },
+    {
+      "id": 2,
+      "name": "Bob Smith",
+      "position": "Product Manager",
+      "email": "bob@techcompany.com"
+    },
+    {
+      "id": 3,
+      "name": "Catherine Lee",
+      "position": "UX Designer",
+      "email": "catherine@techcompany.com"
+    },
+    {
+      "id": 4,
+      "name": "David Wilson",
+      "position": "Data Scientist",
+      "email": "david@techcompany.com"
+    }
+  ]
 
-    return new Response(JSON.stringify({ employees }), {
+    return new Response(JSON.stringify({ employees}), {
         status: 200,
         headers: { "Content-Type": "application/json" },
     })
