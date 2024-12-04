d7013a9ea6ff82b109e586d0f97a237935376fdd
SantinoGiordano <santinogiordano13@gmail.com>
2024-12-04 11:09:31 -0500

    fix more styling and changed name

--- app/(root)/product/page.tsx
+++ app/(root)/product/page.tsx
@@ -1,11 +1,17 @@
-export default function Products(){
-    return(
-        <div className="h-screen bg-fixed bg-center bg-cover bg-background-image-products">
-          <div className="h-full bg-black/50 flex items-center justify-center">
-            <h1 className="text-white text-4xl md:text-6xl font-bold">
-              Event Planing
-            </h1>
-          </div>
+
+
+export default function Products() {
+
+  return (
+    <>
+      <div className="h-screen bg-fixed bg-center bg-cover bg-productBackground">
+        <div className="h-full bg-black/50 flex items-center justify-center">
+          <h1 className="text-white text-4xl md:text-6xl font-bold">
+            Products
+          </h1>
         </div>
-    )
+      </div>
+    </>
+  );
+  
 }
