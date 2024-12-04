8cf3aad942f5ed4c0c44dee87bb81c6609c757ad
SantinoGiordano <santinogiordano13@gmail.com>
2024-12-04 11:07:26 -0500

    moved all home page code to about and added gears img

--- app/page.tsx
+++ app/page.tsx
@@ -1,52 +1,15 @@
-"use client";
 
-import { useEffect, useState } from "react";
-
-interface Employee {
-  name: string;
-  position: string;
-  email: string;
-}
 
 export default function Home() {
-  const [employees, setEmployees] = useState<Employee[]>([]); // Initialize as an empty array
 
-  async function getEmployees() {
-    try {
-      const response = await fetch("http://localhost:3000/api/employees");
-      const json = await response.json();
-      setEmployees(json.employees || []);//this was origanlly json.data and i needed to change, this was my big change 
-
-      console.log({ json });
-    } catch (error) {
-      console.error(error);
-      setEmployees([]); // Fallback to empty array on error
-    }
-  }
-
-  useEffect(() => {
-    getEmployees();
-    console.log("Employees after fetch:", employees);
-  }, []);
-
-  function getNames() {
-    console.log(employees.map((employee) => employee.name)); // Log employee names
-  }
 
   return (
-    <div>
-      <button onClick={getNames}>Click me</button>
-      <h1>Employee List</h1>
-      <div>
-        {employees.map((employee, index) => (
-          <div key={index} className="employee-card">
-            <h2>{employee.name}</h2>
-            <p>{employee.position}</p>
-            <p>{employee.email}</p>
-          </div>
-        ))}
-        {!employees.length && <p>loading...</p>}
-      </div>
+    <div className="h-screen bg-fixed bg-center bg-cover bg-homeBackground">
+    <div className="h-full bg-black/50 flex items-center justify-center">
+      <h1 className="text-white text-6xl md:text-6xl font-bold">
+        Software Solutions
+      </h1>
     </div>
+  </div>
   );
 }
