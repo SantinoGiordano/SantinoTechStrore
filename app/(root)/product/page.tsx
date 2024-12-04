ad5c055b15aa1f596b16915bd2fd1378620254f1
SantinoGiordano <santinogiordano13@gmail.com>
2024-12-04 13:07:48 -0500

    transfred all data and mapped, added daisy card, added useeffect, added fetch

--- app/(root)/product/page.tsx
+++ app/(root)/product/page.tsx
@@ -1,15 +1,63 @@
+"use client";
 
+import React, { useEffect, useState } from "react";
+
+interface Services {
+  id: number;
+  name: string;
+  description: string;
+  price: number;
+  img: string;
+}
 
 export default function Products() {
-    return (
-      <>
-        <div className="h-screen bg-fixed bg-center bg-cover bg-productBackground">
-          <div className="h-full bg-black/50 flex items-center justify-center">
-            <h1 className="text-white text-4xl md:text-6xl font-bold">
-              Products
-            </h1>
-          </div>
-        </div>
-      </>
-    );
+  const [services, setServices] = useState<Services[]>([]);
+
+  async function getServices() {
+    try {
+      const response = await fetch("http://localhost:3000/api/services");
+      const json = await response.json();
+      setServices(json.services || []);
+      console.log({ json });
+    } catch (error) {
+      console.error(error);
+      setServices([]);
+    }
   }
+
+  useEffect(() => {
+    getServices();
+    console.log("Services after fetch:", services);
+  }, []);
+
+  return (
+    <>
+    <div className="bg-blue-500">
+      <div className="h-screen bg-fixed bg-center bg-cover bg-productBackground">
+        <div className="h-full bg-black/50 flex items-center justify-center">
+          <h1 className="text-white text-4xl md:text-6xl font-bold">
+            Products
+          </h1>
+        </div>
+      </div>
+
+      <div className="flex flex-wrap gap-5 justify-center p-10 text-white">
+        {services.map((service, index) => (
+          <div key={index} className="bg-blue-950 card card-side bg-base-100 shadow-xl transform transition-all duration-300 hover:scale-105">
+            <div className="flex flex-col max-w-72">
+              <figure className="p-2">
+                <img src={service.img} alt={service.name} />
+              </figure>
+              <div className="card-body">
+                <h2 className="card-title font-bold p-2">{service.name}</h2>
+                <p className="p-2 font-serif font-thin">{service.description}</p>
+                <div className="card-actions justify-end p-2">${service.price}.99</div>
+              </div>
+            </div>
+          </div>
+        ))}
+      </div>
+    </div>
+    </>
+  );
+}
