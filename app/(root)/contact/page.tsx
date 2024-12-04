e1249ea6f6a96b56c09a975efae8d053a4493970
SantinoGiordano <santinogiordano13@gmail.com>
2024-12-04 11:08:44 -0500

    put scrolling on products page

---
+++ app/(root)/contact/page.tsx
@@ -0,0 +1,12 @@
+export default function Products(){
+    return(
+        <div className="h-screen bg-fixed bg-center bg-cover bg-productBackground"
+        style={{ backgroundImage: "url('/product.jpg')" }}>
+          <div className="h-full bg-black/50 flex items-center justify-center">
+            <h1 className="text-white text-4xl md:text-6xl font-bold">
+              Products
+            </h1>
+          </div>
+        </div>
+    )
+}
