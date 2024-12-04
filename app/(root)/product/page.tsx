82c8f649ba995adf5c81cd913d609092d5303f8f
SantinoGiordano <santinogiordano13@gmail.com>
2024-12-04 11:55:34 -0500

    removed some white space

--- app/(root)/product/page.tsx
+++ app/(root)/product/page.tsx
@@ -1,17 +1,15 @@
 
 
 export default function Products() {
-
-  return (
-    <>
-      <div className="h-screen bg-fixed bg-center bg-cover bg-productBackground">
-        <div className="h-full bg-black/50 flex items-center justify-center">
-          <h1 className="text-white text-4xl md:text-6xl font-bold">
-            Products
-          </h1>
+    return (
+      <>
+        <div className="h-screen bg-fixed bg-center bg-cover bg-productBackground">
+          <div className="h-full bg-black/50 flex items-center justify-center">
+            <h1 className="text-white text-4xl md:text-6xl font-bold">
+              Products
+            </h1>
+          </div>
         </div>
-      </div>
-    </>
-  );
-  
-}
+      </>
+    );
+  }
