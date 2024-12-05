b99f9ac9a9900282c4eb6ef968fbd08e034bc09c
SantinoGiordano <santinogiordano13@gmail.com>
2024-12-05 12:57:46 -0500

    fake pages/ back up pages for products

---
+++ app/(product items)/Custom-Software-Development/page.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <div className='pt-96'>this is a tezt
+    </div>
+  )
+}
+
+export default page
---
+++ app/(product items)/Mobile-App-Development/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <div className='pt-96'>helllo</div>
+  )
+}
+
+export default page
