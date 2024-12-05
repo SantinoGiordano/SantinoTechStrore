fe675301344268671555d80da764e7048a2023dd
SantinoGiordano <santinogiordano13@gmail.com>
2024-12-05 12:56:56 -0500

    added a bunch of filler text to main page

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,49 @@
-
-
 export default function Home() {
-
-
   return (
-    <div className="h-screen bg-fixed bg-center bg-cover bg-homeBackground">
-    <div className="h-full bg-black/50 flex items-center justify-center">
-      <h1 className="text-white text-6xl md:text-6xl font-bold">
-        Software Solutions
-      </h1>
-    </div>
-  </div>
+    <>
+      <div className="bg-red-400">
+        <div className="h-screen bg-fixed bg-center bg-cover bg-homeBackground">
+          <div className="h-full bg-black/50 flex items-center justify-center">
+            <h1 className="text-white text-6xl md:text-6xl font-bold">
+              Software Solutions
+            </h1>
+          </div>
+        </div>
+        <div className="ml-[15%] mr-[15%] text-justify text-white ">
+          <div className="p-10 ">
+          
+            Welcome to Software Solutions, a place that can streamline your
+            operations, enhance productivity, and drive growth. Our team of
+            experts is committed to delivering cutting-edge technologies
+            designed to meet the unique needs of your business, whether you're a
+            startup or an established enterprise.
+          </div>
+          <div className="p-10">
+            <p className="text-center text-4xl p-5">Why Choose Us?</p>
+            Tailored Solutions: We understand that each business is unique.
+            That’s why we offer customizable software that can be seamlessly
+            integrated into your existing processes. Scalable & Future-Proof:
+            Our solutions are built to grow with your business. We ensure your
+            software evolves as your needs change and as technology advances.
+            Cutting-Edge Technology: Leveraging the latest in AI, machine
+            learning, and cloud computing, we provide solutions that keep you
+            ahead of the curve. Expert Support: Our dedicated support team is
+            here to ensure a smooth transition, continuous improvements, and
+            immediate assistance whenever you need it.
+            <p className="text-center text-4xl p-5">What Separates Us?</p>
+            We believe that every business deserves a solution that fits its
+            specific challenges, which is why we take a collaborative approach
+            to every project. Our comprehensive services span from custom
+            software development and enterprise solutions to cloud-based
+            applications and mobile app development. At Software Solutions, we
+            are committed to delivering high-quality products that streamline
+            operations and empower businesses to thrive in a rapidly evolving
+            digital landscape. With our focus on innovation and customer
+            satisfaction, we ensure that our clients always stay ahead of the
+            curve.
+          </div>
+        </div>
+      </div>
+    </>
   );
 }
