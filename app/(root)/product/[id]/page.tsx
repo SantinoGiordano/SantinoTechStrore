11ffd9d0903215452980dc5bfb3de147ba1500b8
SantinoGiordano <santinogiordano13@gmail.com>
2024-12-06 10:42:15 -0500

    reorginized  [id] and added back button

--- app/(root)/product/[id]/page.tsx
+++ app/(root)/product/[id]/page.tsx
@@ -1,6 +1,7 @@
-'use client'
-import { useParams, useRouter } from 'next/navigation'
-import React, { useEffect, useState } from 'react'
+"use client";
+import Link from "next/link";
+import { useParams, useRouter } from "next/navigation";
+import React, { useEffect, useState } from "react";
 
 interface Services {
   id: number;
@@ -10,8 +11,7 @@ interface Services {
   img: string;
 }
 
-
-export default function Page(){
+export default function Page() {
   const [services, setServices] = useState<Services[]>([]);
 
   async function getServices() {
@@ -30,7 +30,6 @@ export default function Page(){
     console.log("Services after fetch:", services);
   }, []);
 
-
   const { id } = useParams(); // Use useParams instead of useRouter
 
   // Convert the id to a number (since the URL parameter is a string)
@@ -45,12 +44,17 @@ export default function Page(){
   }
 
   return (
-    <div className='pt-96'>
-      <h1>Product Details</h1>
-      <div>You clicked on: {product.name}</div>
-      <div>Description: {product.description}</div>
-      <div>Price: ${product.price}</div>
-      <img src={product.img} alt={product.name} />
-    </div>
+    <>
+      <div className="text-center pt-96">
+        <img src={product.img} alt={product.name} className="mx-auto" />
+        <h1>Product Details</h1>
+        <div>{product.name}</div>
+        <div className=''>Description: {product.description}</div>
+        <div>Price: ${product.price}</div>
+        <Link href={"/product"}>
+        <button type="button" className="min-w-96 text-gray-900 hover:text-white border border-gray-800 hover:bg-gray-900 focus:ring-4 focus:outline-none focus:ring-gray-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2 dark:border-gray-600 dark:text-gray-400 dark:hover:text-white dark:hover:bg-gray-600 dark:focus:ring-gray-800">Dark</button>
+        </Link>
+      </div>
+    </>
   );
 }
