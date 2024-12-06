74be9de14b1360d0c7fbcf99c80bf560f03a558d
SantinoGiordano <santinogiordano13@gmail.com>
2024-12-06 12:32:01 -0500

    start for error handling

---
+++ app/componets/productError.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function ProductError(){
+  return (
+    <div className='text-center pt-56 text-5xl font-bold'>
+    <div>There has been an error, please try reloading the page :/</div>
+    <p>Thank you!</p>
+    </div>
+  )
+}
