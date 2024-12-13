b7351107f3ffb79ce1ea6fe3d8debeef0a6d5cc7
SantinoGiordano <santinogiordano13@gmail.com>
2024-12-12 21:54:19 -0500

    added spinner

--- app/(root)/loading.tsx
+++ app/(root)/loading.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Loading = () => {
   return (
-    <span className="loading loading-spinner loading-lg"></span>
+    <span className="loading loading-spinner loading-lg">Loading</span>
   )
 }
 
