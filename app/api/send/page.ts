287847448173dd86491119cccd2188c6e414f212
SantinoGiordano <santinogiordano13@gmail.com>
2024-12-12 21:53:58 -0500

    commneted out unsed immports

--- app/api/send/page.ts
+++ app/api/send/page.ts
@@ -1,6 +1,6 @@
 import { Resend } from 'resend';
-import fs from 'fs';
-import path from 'path';
+// import fs from 'fs';
+// import path from 'path';
 
 const resend = new Resend(process.env.RESEND_API_KEY);
 
